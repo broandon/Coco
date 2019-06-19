0de5ec12c233f6a718542ebad666578ce3d27de3
Carlos Banos <cbanos@travelbank.com>
2019-06-18 19:04:31 -0500

    Initial Commit

---
+++ Coco/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coco
+//
+//  Created by Carlos Banos on 6/18/19.
+//  Copyright © 2019 Easycode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
